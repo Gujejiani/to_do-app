801a946917cacf34e9373950b3d51e3b252c8cd4
Kakha Gujejiani <67228459+Gujejiani@users.noreply.github.com>
2021-01-27 07:38:43 -0800

    status modal added

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ const navStyle = JSON.parse(localStorage.getItem("nav_style"));
 const bodyStyle = JSON.parse(localStorage.getItem("body_style"));
 const checked = JSON.parse(localStorage.getItem("checked"));
 //checking if theme styles is already added to local Storage, if so than adding them to elements
+
 if (navStyle && bodyStyle && checked) {
   addStylesFromLocalStorage();
 }
@@ -87,37 +88,80 @@ function addStylesFromLocalStorage() {
     }
   });
 }
-////////////////////////////////////////// Themes End /////////////////////////////////////////////////////
+////////////////////////////////////////// Theme End /////////////////////////////////////////////////////
+
 const createTask = document.querySelector(".create_task");
 const tasks_Wrapper = document.querySelector(".tasks_wrapper");
 const tasksTitle = document.querySelectorAll(".task-title");
 
-let heightadded = false;
+const formContainer = document.querySelector(".form_container");
+
+const taskInput = document.querySelector(".form_about-task");
+const descriptionInput = document.querySelector(".form_about-description");
+const task_types = document.querySelector(".form_about_type");
+const formSubmit = document.querySelector(".form");
+const types = document.querySelectorAll(".type");
+
+const overlay = document.querySelector(".overlay");
+const task_overlay = document.querySelector(".task-overlay");
+const setType = document.querySelector(".set-type");
+
+const statusModal = document.querySelector(".status_wrapper");
+const statusSelectedICons = document.querySelectorAll(".status_icon");
+const statusType = document.querySelector(".set-type");
+
+const priorityModal = document.querySelector(".priority_wrapper");
+const prioritySelectedIcons = document.querySelectorAll(".priority_icon");
+const setPriorityType = document.querySelector(".set-priority");
+
+const prioritySave = document.querySelector(".priority-save");
+const priorityCancel = document.querySelector(".priority-cancel");
 
 class App {
   #id;
+  #hideForm;
+  #type;
+  #status;
+  #priority;
+  #priorityColor;
+  #tasks = [];
+
   constructor() {
-    // todayTasks.addEventListener("click", this.toggleTodayTasks);
-    // tomorrowTasks.addEventListener("click", this.toggleTodayTasks);
     tasks_Wrapper.addEventListener("click", this._showAndHidetasks.bind(this));
+    createTask.addEventListener("click", this._toggleOverlayForm.bind(this));
+    formSubmit.addEventListener("submit", this._newTaskAdded.bind(this));
+    task_overlay.addEventListener("click", this._hideForm.bind(this));
+    task_types.addEventListener("click", this._taskTypeHandler.bind(this));
+    setType.addEventListener("click", this._typeSelectHandler);
+    overlay.addEventListener("click", this._overlayClicked);
+    statusModal.addEventListener("click", this._selectStatus.bind(this));
+    priorityModal.addEventListener("click", this._selectPriority.bind(this));
+    prioritySave.addEventListener("click", this._savePriority.bind(this));
+    setPriorityType.addEventListener("click", this._priorityModal.bind(this));
+    priorityCancel.addEventListener("click", this._priorityCancel.bind(this));
+  }
+
+  _newTaskAdded(e) {
+    e.preventDefault();
+
+    this._hideForm();
   }
 
   _showAndHidetasks(e) {
     const title = e.target;
-    const items = title.nextSibling;
+    const task = title.nextSibling;
     //checking if title clicked
     if (title.nodeName === "H3") {
       this.#id = title.dataset.id;
-
       title.classList.toggle("task_title_clicked");
       const taskUlList = document.querySelector(`.${this.#id}`);
 
       let tasksCount = taskUlList.childElementCount;
-      items.textContent = tasksCount;
+      task.textContent = tasksCount;
       //  counting ul height, acording to children
       const height = 35 * tasksCount;
 
-      this._removeTitleSelectors(this.#id);
+      this._removeTitleSelectors(tasksTitle, this.#id, "task_title_clicked");
 
       //checking if heigth is added or not to ul element, which cotains our task lists
       if (taskUlList.offsetHeight === 0) {
@@ -129,13 +173,159 @@ class App {
   }
 
   //removing color from past selected task titles
-  _removeTitleSelectors(id) {
-    tasksTitle.forEach((title) => {
+  _removeTitleSelectors(el, id, className) {
+    el.forEach((title) => {
+      // console.log(title.dataset.id, id, className);
       if (title.dataset.id !== id) {
-        title.classList.remove("task_title_clicked");
+        title.classList.remove(`${className}`);
       }
     });
+  }
+
+  _toggleOverlayForm(e) {
+    console.log(e.target);
+    task_overlay.classList.toggle("task_overlay-show");
+    console.log(task_overlay);
+    formContainer.classList.toggle("form_modal-Show");
+    setTimeout(() => {
+      taskInput.focus();
+    }, 30);
+  }
+  _hideForm(e) {
+    console.log(e.target);
+    //checking if closest node is from element
+
+    if (!e.target) return;
+
+    formContainer.classList.remove("form_modal-Show");
+    task_overlay.classList.remove("task_overlay-show");
+    this._removeTitleSelectors(types, "all", "task_type-selected");
+
+    // if (formContainer.classList.contains("form_modal-Show")) {
+    //   //if overlay is already show and target is not form, we are hiding form overlay
+    //   console.log("hid Form");
+    //   formContainer.classList.remove("form_modal-Show");
+    //   descriptionInput.value = taskInput.value = "";
+    //   this.#hideForm = false;
+    //   //remove selected types
+
+    // } else {
+    //   this.#hideForm = true;
+    // }
+  }
+
+  _taskTypeHandler(e) {
+    const type = e.target;
+
+    if (type.classList.contains("type")) {
+      type.classList.add("task_type-selected");
+      this.#type = type.dataset.id;
+
+      this._removeTitleSelectors(types, this.#type, "task_type-selected");
+    }
+  }
+  _selectStatus(e) {
+    //checking if list of status clicked
+    if (e.target.nodeName === "LI") {
+      let list = e.target;
+      this.#status = list.dataset.status;
+      // by children finding icon element
+
+      let selecTedIcon = list.children[0];
+      //changing style to visibility visible
+      selecTedIcon.classList.toggle("status_selected");
+
+      this._removeTitleSelectors(
+        statusSelectedICons,
+        this.#status,
+        "status_selected"
+      );
+      statusType.textContent = this.#status;
+      console.log("245 overlay");
+      this._overlayClicked();
+    }
+  }
+  _savePriority() {
+    if (this.#priority) {
+      this._getPriorityColor(this.#priority);
+
+      statusModal.classList.remove("status-show");
+      setPriorityType.innerHTML = `${this.#priority}`;
+      setPriorityType.style.background = `${this.#priorityColor}`;
+      priorityModal.classList.remove("priority-show");
+      this._overlayClicked();
+    }
+  }
+  _priorityCancel() {
+    this._resetPriority();
+  }
+
+  _resetPriority() {
+    this.#priority = "";
+    this.#priorityColor = "";
+    statusModal.classList.remove("status-show");
+    this._overlayClicked();
+  }
+
+  _selectPriority(e) {
+    if (e.target.nodeName === "LI") {
+      let list = e.target;
+      this.#priority = list.dataset.priority;
+
+      let selecTedIcon = list.children[0];
+      //changing style to visibility visible
+      selecTedIcon.classList.toggle("priority_selected");
+      this._removeTitleSelectors(
+        prioritySelectedIcons,
+        this.#priority,
+        "priority_selected"
+      );
+      console.log(this.#priority);
+    }
+  }
+
+  _typeSelectHandler() {
+    overlay.classList.add("show");
+    statusModal.classList.add("status-show");
+  }
+  _priorityModal() {
+    console.log("asdda");
+    overlay.classList.add("show");
+    priorityModal.classList.add("priority-show");
+  }
+  _overlayClicked(e) {
+    statusModal.classList.remove("status-show");
+    priorityModal.classList.remove("priority-show");
+    overlay.classList.remove("show");
+  }
+  _getPriorityColor(priority) {
+    switch (priority) {
+      case "Important":
+        this.#priorityColor = "#965657";
+        break;
+      case "Family":
+        this.#priorityColor = "#c96567";
+        break;
+      case "Deadline":
+        this.#priorityColor = "#e8a87c";
+        break;
+      case "Fun":
+        this.#priorityColor = "#85dccb";
+        break;
+      default:
+        console.log("no match");
+        break;
+    }
   }
 }
 
 const app = new App();
+
+class Task {
+  constructor(task, description, type, priority) {
+    this.task = task;
+    this.description = description;
+    this.type = type;
+    this.priority = priority;
+  }
+}
